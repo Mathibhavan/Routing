8ed3364470cb96107eb85d503f502813f2246074
Mathi <mlatha2896@gmail.com>
2023-07-09 19:30:15 +0530

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path: 'home', component: HomeComponent} ,
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
